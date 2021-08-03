6f64cfcbd837fee4da40fda138a29552419a278b
Esra Ozdem <esraozdem43@gmail.com>
2021-08-03 17:12:59 -0400

    first comment

---
+++ src/test/java/DDTestUsingJSON.java
@@ -0,0 +1,94 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class DDTestUsingJSON {
+
+    WebDriver driver;
+    @BeforeClass
+   public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test(dataProvider="dp")
+
+    public void login(String data){
+
+       String[]  users = data.split(",");
+
+        driver.get("https://demo.nopcommerce.com/login");
+
+        driver.findElement(By.id("Email")).sendKeys(users[0]);
+
+        driver.findElement(By.id("Password")).sendKeys(users[1]);
+
+        driver.findElement(By.xpath("//button[@class='button-1 login-button']")).click();
+        String act_title= driver.getTitle();
+
+        String exp_title= "nopCommerce demo store. Login";
+
+        Assert.assertEquals(act_title,exp_title);
+
+
+
+    }
+
+    @DataProvider(name="dp")
+
+    public Object[] readJson() throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        FileReader reader = new FileReader("./jsonfiles/testdata.json");
+     Object obj=   jsonParser.parse(reader);
+
+    JSONObject userloginsJsonobj=    (JSONObject)obj;
+
+     JSONArray   userloginsArray= (JSONArray)userloginsJsonobj.get("userlogins");
+
+     String[] arr = new String[userloginsArray.size()];
+
+        for (int i = 0; i < userloginsArray.size() ; i++) {
+
+            JSONObject users = (JSONObject)userloginsArray.get(i);
+
+           String user = (String) users.get("username");
+
+           String pwd = (String)users.get("password");
+
+           arr[i] = user + " ," + pwd;
+
+        }
+
+        return arr;
+
+
+
+
+    }
+}
---
+++ src/test/java/ReadDataFromJSONFile.java
@@ -0,0 +1,54 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadDataFromJSONFile {
+
+
+    public static void main(String[] args) throws IOException, ParseException {
+     JSONParser jsonParser = new JSONParser();
+     FileReader fileReader = new FileReader("./jsonfiles/employee.json");
+
+   Object obj= jsonParser.parse(fileReader);
+
+
+        JSONObject emptyJsonObj =(JSONObject)obj;
+
+        String fname = (String)emptyJsonObj.get("firstname");
+
+        String lname= (String)emptyJsonObj.get("lastname");
+
+        JSONArray array = (JSONArray)emptyJsonObj.get("address");
+
+        for (int i = 0; i < array.size(); i++) {
+
+
+            JSONObject address = (JSONObject)array.get(i);
+
+            String street = (String)address.get("street");
+            String city = (String)address.get("city");
+
+            String  state = (String)address.get("state");
+
+            System.out.println("Address of " + i + "");
+            System.out.println(street);
+            System.out.println(city);
+            System.out.println(state);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
